c8e26debbe85055ffd81d70d729743c3ca121ada
Hannes Schaller <hannes.schaller@anovis.com>
2017-10-11 13:07:08 +0200

    add comment action

---
+++ actions/add_comment.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import json
+
+from lib.icinga2action import Icinga2Action
+
+
+class Icinga2AddComment(Icinga2Action):
+
+    def run(self, object_type="Host", filter_string="", filter_vars="", author="", comment=""):
+        client = self.get_client()
+        #TODO: an empty result is still ok.
+        return json.dumps(client.actions.add_comment(object_type=object_type, filter=filter_string, filter_vars=filter_vars, author=author, comment=comment))
